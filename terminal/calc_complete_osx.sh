0f44244ecda3e182ff4a4490e06fbf835b469498
Adwait Dongare <adongare@cmu.edu>
2015-10-08 01:50:57 -0400

    added OSX terminal versions

---
+++ terminal/calc_complete_osx.sh
@@ -0,0 +1,5 @@
+# open cc_message.sh in a different terminal
+# only works on OSX
+
+# if your message file is elsewhere, change the path to point ot it
+open -a Terminal ./cc_message.sh
---
+++ terminal/cc_message.sh
@@ -0,0 +1,6 @@
+printf '\e[8;5;40t' # resize terminal to 40x5
+clear               # clear screen
+echo                # blank line
+echo                # blank line
+echo "        CALCULATION CORRECT"  # payload
+read                # wait for user toterminate
